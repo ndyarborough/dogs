0191ad25ebb52d2954cd0af9002e70fdf706a620
nyarbor1 <113961150+nyarbor1@users.noreply.github.com>
2025-02-12 19:10:01 -0500

    style fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-gradient text-secondary max-w-dvw max-h-svw">
+      <body className="bg-gradient text-secondary max-w-dvw max-h-dvh">
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
